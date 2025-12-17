ec88fab94b40ce778fa57aa33d5698f161bbce60
CaptainLevi17 <migz.levi@gmail.com>
2025-12-17 23:09:41 +1100

    Expense Tracker design and layout done

---
+++ main.py
@@ -0,0 +1,55 @@
+# Imports
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QVBoxLayout, QLabel, QHBoxLayout, QLineEdit, QTableWidget, QTableWidgetItem, QDateEdit, QComboBox
+
+# Objects
+class ExpenseTracker(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Expense Tracker")
+        self.resize(550, 500)
+        
+        self.date_box = QDateEdit()
+        self.dropdown = QComboBox()
+        self.amount = QLineEdit()
+        self.description = QLineEdit()
+
+        self.add_button = QPushButton("Add Expense")
+        self.delete_button = QPushButton("Delete Expense")
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(5)
+        self.table.setHorizontalHeaderLabels(["ID"," Date", "Category", "Amount", "Description"])
+        
+# Layout
+        self.master_layout = QVBoxLayout()
+        self.row1 = QHBoxLayout()
+        self.row2 = QHBoxLayout()
+        self.row3 = QHBoxLayout()
+
+        self.row1.addWidget(QLabel("Date:"))
+        self.row1.addWidget(self.date_box)
+        self.row1.addWidget(QLabel("Category:"))
+        self.row1.addWidget(self.dropdown)
+
+        self.row2.addWidget(QLabel("Amount:"))
+        self.row2.addWidget(self.amount)
+        self.row2.addWidget(QLabel("Description:"))
+        self.row2.addWidget(self.description)
+
+        self.row3.addWidget(self.add_button)
+        self.row3.addWidget(self.delete_button)
+
+        self.master_layout.addLayout(self.row1)
+        self.master_layout.addLayout(self.row2)
+        self.master_layout.addLayout(self.row3)
+        self.master_layout.addWidget(self.table)
+
+        self.setLayout(self.master_layout)
+
+# Execution
+if __name__ == "__main__":
+    app = QApplication([])
+    main = ExpenseTracker()
+    main.show()
+    app.exec_()
+
